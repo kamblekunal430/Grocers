c9d27fa7782d3ee7607396b409dde13cc4f3f404
kamblekunal430 <kamblekunal430@gmail.com>
2021-09-20 13:03:33 +0530

    defined the cart schema

---
+++ server/models/Cart.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// defining cart schema
+
+const CartSchema = new Schema({
+  userId: {
+    type: String,
+  },
+  items: [
+    {
+      itemId: {
+        type: String,
+      },
+      name: {
+        type: String,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        min: [1, "Quantity cannot be less than 1"],
+        default: 1,
+      },
+      bill: {
+        type: Number,
+        required: true,
+        default: 0,
+      },
+    },
+  ],
+});
+
+module.exports = Cart = mongoose.model("cart", CartSchema);
