85c178a68c55b11fbfe4912febeb0a113a11a633
kamblekunal430 <kamblekunal430@gmail.com>
2021-09-20 12:52:00 +0530

    defined the items schema

---
+++ server/models/Item.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// defining item schema
+
+const ItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  details: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  dateAdded: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Item = mongoose.model("item", ItemSchema);
