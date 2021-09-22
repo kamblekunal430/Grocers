770e9a17d3e9686a33bf1d41e25d6255c5cf81f0
kamblekunal430 <kamblekunal430@gmail.com>
2021-09-22 11:40:22 +0530

    added route to add item to cart from the wish list

--- server/routes/wishlistRoute.js
+++ server/routes/wishlistRoute.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get("/wishlist/:id", wishlistController.get_wishlist_items);
 router.post("/wishlist/:id", wishlistController.post_wishlist_item);
+router.post("/wishlist/:userId/:itemId", wishlistController.add_to_cart_item);
 router.delete("/wishlist/:userId/:itemId", wishlistController.delete_item);
 
 module.exports = router;
