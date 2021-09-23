87ee6959cd753d6326dcf7daa9aba38c47353792
kamblekunal430 <kamblekunal430@gmail.com>
2021-09-23 12:39:22 +0530

    created wishlist actions to get add and delete items from wishlist

---
+++ client/src/actions/wishlistActions.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { getErrors } from "./errorActions";
+import {
+  GET_WISHLIST_ITEMS,
+  POST_WISHLIST_ITEM,
+  DELETE_FROM_WISHLIST,
+  WISHLIST_LOADING,
+} from "./types";
+
+// getting wishlist items
+export const getWishlistItems = (id) => (dispatch) => {
+  dispatch(setWishlistLoading());
+  axios
+    .get(`/api/wishlist/${id}`)
+    .then((res) =>
+      dispatch({
+        type: GET_WISHLIST_ITEMS,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch(getErrors(err.response.data, err.response.status))
+    );
+};
+
+// adding items to the wishlist
+
+export const postWishlistItem = (id, itemId) => (dispatch) => {
+  axios
+    .post(`/api/wishlist/${id}`, { itemId })
+    .then((res) =>
+      dispatch({
+        type: POST_WISHLIST_ITEM,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch(getErrors(err.response.data, err.response.status))
+    );
+};
+
+// deleting item from the wishlist
+
+export const deleteItem = (userId, itemId) => (dispatch) => {
+  axios
+    .delete(`/api/wishlist/${userId}/${itemId}`)
+    .then((res) =>
+      dispatch({
+        type: DELETE_FROM_WISHLIST,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch(getErrors(err.response.data, err.response.status))
+    );
+};
+
+export const setWishlistLoading = () => {
+  return {
+    type: WISHLIST_LOADING,
+  };
+};
