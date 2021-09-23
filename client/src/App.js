67e463df68d0e285138a0f0f50073e7f8a97701f
kamblekunal430 <kamblekunal430@gmail.com>
2021-09-23 20:26:25 +0530

    created navbar which contain the Login Modal, Register modal, Logout button, and links to various pages on our website.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import LoginModal from "./components/auth/LoginModal";
-
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
-      <LoginModal></LoginModal>
     </div>
   );
 }
