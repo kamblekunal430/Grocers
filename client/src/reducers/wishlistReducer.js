d83d89d500304a104021cfaefc0dbe68f5bfa542
kamblekunal430 <kamblekunal430@gmail.com>
2021-09-23 16:57:18 +0530

    created wishlistReducer to deal with reducer related to the wishlist of the user

---
+++ client/src/reducers/wishlistReducer.js
@@ -0,0 +1,39 @@
+import {
+  GET_WISHLIST_ITEMS,
+  POST_WISHLIST_ITEM,
+  DELETE_FROM_WISHLIST,
+  WISHLIST_LOADING,
+} from "../actions/types";
+
+const initialState = {
+  loading: false,
+  wishlist: null,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_WISHLIST_ITEMS:
+      return {
+        ...state,
+        loading: false,
+        wishlist: action.payload,
+      };
+    case POST_WISHLIST_ITEM:
+      return {
+        ...state,
+        wishlist: action.payload,
+      };
+    case DELETE_FROM_WISHLIST:
+      return {
+        ...state,
+        wishlist: action.payload,
+      };
+
+    case WISHLIST_LOADING: {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+  }
+}
