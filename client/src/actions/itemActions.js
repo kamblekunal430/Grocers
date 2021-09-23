4d009e8f48b990f5083fb22cec1ff34a2ae1d584
kamblekunal430 <kamblekunal430@gmail.com>
2021-09-23 12:10:18 +0530

    created itemsAction to add update get and delete items

---
+++ client/src/actions/itemActions.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+import {
+  GET_ITEMS,
+  POST_ITEM,
+  DELETE_ITEM,
+  UPDATE_ITEM,
+  ITEMS_LOADING,
+} from "./types";
+import { getErrors } from "./errorActions";
+
+// getting all the items from the backend
+
+export const getItems = () => (dispatch) => {
+  dispatch(setItemsLoading());
+  axios
+    .get("/api/items")
+    .then((res) =>
+      dispatch({
+        type: GET_ITEMS,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch(getErrors(err.response.data, err.response.status))
+    );
+};
+
+//addding the new items
+
+export const postItem = (item) => (dispatch) => {
+  axios
+    .post("/api/items", item)
+    .then((res) =>
+      dispatch({
+        type: POST_ITEM,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch(getErrors(err.response.data, err.response.status))
+    );
+};
+
+// updating the item
+
+export const updateItem = (id, item) => (dispatch) => {
+  axios
+    .put(`/api/items/${id}`, item)
+    .then((res) =>
+      dispatch({
+        type: UPDATE_ITEM,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch(getErrors(err.response.data, err.response.status))
+    );
+};
+
+// deleting the items
+
+export const deleteItem = (id) => (dispatch) => {
+  axios
+    .delete(`/api/items/${id}`)
+    .then((res) =>
+      dispatch({
+        type: DELETE_ITEM,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch(getErrors(err.response.data, err.response.status))
+    );
+};
+
+export const setItemsLoading = () => {
+  return {
+    type: ITEMS_LOADING,
+  };
+};
