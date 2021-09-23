8d9c8fc99b022fb803752531b87cd6ffe0afdc5a
kamblekunal430 <kamblekunal430@gmail.com>
2021-09-23 20:11:14 +0530

    created the authntication component such as register login logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import LoginModal from "./components/auth/LoginModal";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <LoginModal></LoginModal>
     </div>
   );
 }
