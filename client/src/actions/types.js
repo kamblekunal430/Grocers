76b57da63e0f6ad8551d6354bd45d1c85598f9c4
kamblekunal430 <kamblekunal430@gmail.com>
2021-09-23 09:51:43 +0530

    created type.js to define the types of action we will be performing

---
+++ client/src/actions/types.js
@@ -0,0 +1,31 @@
+// items
+export const GET_ITEMS = "GET_ITEMS";
+export const POST_ITEM = "POST_ITEM";
+export const DELETE_ITEM = "DELETE_ITEM";
+export const UPDATE_ITEM = "UPDATE_ITEM";
+export const ITEMS_LOADING = "ITEMS_LOADING";
+
+//cart
+export const CART_LOADING = "CART_LOADING";
+export const GET_CART_ITEMS = "GET_CART_ITEMS";
+export const POST_CART_ITEM = "POST_CART_ITEM";
+export const DELETE_FROM_CART = "DELETE_FROM_CART";
+
+//order
+export const ORDERS_LOADING = "ORDERS_LOADING";
+export const GET_ORDERS = "GET_ORDERS";
+export const POST_ORDER = "POST_ORDER";
+
+//user
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+
+//errors
+export const GET_ERRORS = "GET_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
