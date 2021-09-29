05457125513ffb1bcc57fafdd5edf320f4343d71
kamblekunal430 <kamblekunal430@gmail.com>
2021-09-29 19:29:19 +0530

    fixed the non admin user access to the add item module by redirection

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -12,6 +12,7 @@ import { connect } from "react-redux";
 import { postItem } from "../actions/itemActions";
 import PropTypes from "prop-types";
 import AppNavbar from "./AppNavbar";
+import { Redirect } from "react-router-dom";
 
 class AddItem extends Component {
   state = {
@@ -23,6 +24,7 @@ class AddItem extends Component {
 
   static propTypes = {
     isAuthenticated: PropTypes.bool,
+    user: PropTypes.object.isRequired,
   };
 
   onChange = (e) => {
@@ -49,51 +51,56 @@ class AddItem extends Component {
       <div>
         <AppNavbar />
         <Container>
+          {/* {console.log("user", this.props.user)} */}
           <h2 className="text-center mb-3">Add a Item</h2>
           {this.props.isAuthenticated ? (
-            <Form onSubmit={this.onSubmit}>
-              <FormGroup>
-                <Label for="name">Name</Label>
-                <Input
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder="Name of the item"
-                  onChange={this.onChange}
-                />
-                <br />
-                <Label for="details">Details</Label>
-                <Input
-                  type="text"
-                  name="details"
-                  id="details"
-                  placeholder="Details of the item"
-                  onChange={this.onChange}
-                />
-                <br />
-                <Label for="category">Category</Label>
-                <Input
-                  type="text"
-                  name="category"
-                  id="category"
-                  placeholder="Category of the item"
-                  onChange={this.onChange}
-                ></Input>
-                <br />
-                <Label for="price">Price</Label>
-                <Input
-                  type="number"
-                  name="price"
-                  id="price"
-                  placeholder="Price of the item"
-                  onChange={this.onChange}
-                />
+            this.props.user.isAdmin ? (
+              <Form onSubmit={this.onSubmit}>
+                <FormGroup>
+                  <Label for="name">Name</Label>
+                  <Input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Name of the item"
+                    onChange={this.onChange}
+                  />
+                  <br />
+                  <Label for="details">Details</Label>
+                  <Input
+                    type="text"
+                    name="details"
+                    id="details"
+                    placeholder="Details of the item"
+                    onChange={this.onChange}
+                  />
+                  <br />
+                  <Label for="category">Category</Label>
+                  <Input
+                    type="text"
+                    name="category"
+                    id="category"
+                    placeholder="Category of the item"
+                    onChange={this.onChange}
+                  ></Input>
+                  <br />
+                  <Label for="price">Price</Label>
+                  <Input
+                    type="number"
+                    name="price"
+                    id="price"
+                    placeholder="Price of the item"
+                    onChange={this.onChange}
+                  />
 
-                <Button color="info" style={{ marginTop: "2rem" }} block>
-                  <b>Add Item</b>
-                </Button>
-              </FormGroup>
-            </Form>
+                  <Button color="info" style={{ marginTop: "2rem" }} block>
+                    <b>Add Item</b>
+                  </Button>
+                </FormGroup>
+              </Form>
+            ) : (
+              <Redirect to="/" />
+            )
           ) : (
             <Alert className="text-center" color="danger">
               Login to add items!
@@ -108,6 +115,7 @@ class AddItem extends Component {
 const mapStateToProps = (state) => ({
   item: state.item,
   isAuthenticated: state.auth.isAuthenticated,
+  user: state.auth.user,
 });
 
 export default connect(mapStateToProps, { postItem })(AddItem);
