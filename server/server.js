0eabdba815c531bebf635aa961f3661ab1be68e9
kamblekunal430 <kamblekunal430@gmail.com>
2021-10-06 19:45:54 +0530

    made some changes in docker file

--- server/server.js
+++ server/server.js
@@ -14,9 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // used in the production to serve the client files
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
 }
 
